ffa6cb14bd6c1a8cc2248aebd212cd5979fed53f
Anna-Sh <a.shmidova@yandex.ru>
2018-10-07 17:42:15 +0300

    calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,118 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.expression = initialValue.toString();
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        this.expression += " + " + number;
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.expression += " - " + number;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.expression += " * " + number;
+        return this;
+    }
+
+    devide(number) {
+        this.expression += " / " + number;
+        return this;
+    }
+
+    pow(number) {
+        this.expression += " ^ " + number;
+        return this;
+    }
+
+    _toPostfix() {
+        const stack = [];
+        let postfixExp = "";
+        const operators = {
+            "+": 1,
+            "-": 1,
+            "*": 2,
+            "/": 2,
+            "^": 3
+        };
+
+        function parse(expressionArray) {
+
+            if (expressionArray.length === 0) {
+
+                if (stack.length > 0) {
+
+                    while(stack.length > 0) {
+                        postfixExp += stack.pop() + " ";
+                    }
+                }
+
+                return postfixExp;
+            }
+
+            if (expressionArray[0] in operators) {
+                while (stack.length > 0) {
+                    if (operators[expressionArray[0]] <= operators[stack[stack.length-1]]) {
+                        postfixExp += stack.pop() + " ";
+                    } else {
+                        break;
+                    }
+                }
+                stack.push(expressionArray[0]);
+            } else {
+                postfixExp += expressionArray[0] + " "
+            }
+
+            expressionArray = expressionArray.slice(1);
+
+            return parse(expressionArray);
+        }
+
+        return parse(this.expression.split(' ')).trim();
+    }
+
+    _evaluatePostfix() {
+
+        const postfixExpression = this._toPostfix();
+
+        const operators = {
+            '+': (x, y) => x + y,
+            '-': (x, y) => x - y,
+            '*': (x, y) => x * y,
+            '/': (x, y) => x / y,
+            '^': (x, y) => Math.pow(x, y)
+        };
+
+        let stack = [];
+
+        postfixExpression.split(' ').forEach((token) => {
+            if (token in operators) {
+                let [y, x] = [stack.pop(), stack.pop()];
+                stack.push(operators[token](x, y));
+            } else {
+                stack.push(parseFloat(token));
+            }
+        });
+
+        return stack.pop();
+    }
+
+    valueOf() {
+        return this._evaluatePostfix();
+    }
 }
 
 module.exports = SmartCalculator;
+
+
+
+
+
+
+
+
+
